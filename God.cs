a12118868eade4c5caf48a4fcd64ab176ca8dec4
Enzo <enzo.wang55@gmail.com>
2021-03-24 16:31:35 +0000

    Moved exceptions and limitations handling to god.cs

--- God.cs
+++ God.cs
@@ -8,26 +8,29 @@ public class God : Singleton<God>
     public GameObject builderGO;
     private LibraryBuilder builder;
     public GameObject exceptionRaiserGO;
-    ExceptionRaiser exceptionRaiser;
+    private ExceptionRaiser exceptionRaiser;
+
+    private int maxNumberOfFoldersAllowed = 299;
+    private int maxNumberOfFilesInAFolderAllowed = 204;
 
     public override void Awake()
     {
+        architect = Instantiate(architectGO).GetComponent<LibraryArchitect>();
         exceptionRaiser = FindObjectOfType<ExceptionRaiser>();
         exceptionRaiser.ExceptionRaiserInit();
 
-        architect = Instantiate(architectGO).GetComponent<LibraryArchitect>();
-
-        try
+        try // check if generation causes exceptions
         {
             architect.CreateBlueprint();// this generates a blueprint
         }
 
-        catch (UnauthorizedAccessException) 
+        catch (Exception) // this ensures both unauthorisedexceptions and directorynotfound exception are catched
         {
-            Debug.Log("ACCESS DENIED!");
+            exceptionRaiser.folderExceptionRaised = true;
         }
 
-        exceptionRaiser.folderExceptionRaised = true;
+
+        CheckLibraryGenerationLimitations(architect); // checks if number of files and folders are within limits
     }
 
     private void Start()
@@ -38,6 +41,24 @@ public class God : Singleton<God>
         {
             builder = Instantiate(builderGO).GetComponent<LibraryBuilder>();
             builder.Build(); // this gets the architect and reads the blueprint
+        }
+    }
+
+
+    private void CheckLibraryGenerationLimitations(LibraryArchitect arch) 
+    {
+        if (arch.generatedNodeList.Count > maxNumberOfFoldersAllowed)
+        {
+            exceptionRaiser.maxNumberOfFoldersReached = true;
+        }   
+
+        foreach (Node node in arch.generatedNodeList) // files
+        {
+            if (node.listOfFilesNames.Count > maxNumberOfFilesInAFolderAllowed)
+            {
+                exceptionRaiser.maxNumberOfFilesReached = true;
+                break;
+            }
         }
     }
 }
